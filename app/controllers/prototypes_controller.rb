9f05e9b448763a12e74bc51e04512bd1cb793069
HSKINK <yuhikwut@gmail.com>
2021-09-15 19:50:28 +0900

    修正完了2回目

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -26,8 +26,10 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-    binding.pry
     @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to root_path
+    end
   end
 
   def update
